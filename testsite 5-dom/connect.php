ac8195e69e93fe48553976740c1bc42450476e52
Valord01 <dsouza.valentino@mca.christuniversity.in>
2021-04-08 01:26:16 +0530

    Update connect.php

--- testsite 5-dom/connect.php
+++ testsite 5-dom/connect.php
@@ -1,6 +1,6 @@
 <?php
 $MyUsername = "root";  // enter your username for mysql
-$MyPassword = "3263";  // enter your password for mysql
+$MyPassword = "";  // enter your password for mysql
 $MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
 
 $dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
